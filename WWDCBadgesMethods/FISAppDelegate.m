d6ed685a8695997251cd4b80dfecb3f436a41ece
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-05-18 16:33:28 -0400

    Done.

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -21,4 +21,37 @@
  
  */
 
+-(NSString *)badgeForSpeaker:(NSString *)speaker {
+    
+    NSString *result = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+
+    return result;
+    
+}
+-(NSArray *)badgesForSpeakers:(NSArray *)speakers {
+    NSMutableArray *badgesForSpeakers = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i <= [speakers count]-1; i++) {
+        NSString *badgeForSpeaker =[self badgeForSpeaker: speakers[i]];
+                [badgesForSpeakers addObject:badgeForSpeaker];
+    }
+    
+    return badgesForSpeakers;
+}
+
+-(NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers {
+    
+    NSMutableArray *greetingsAndRoomAssignmentsForSpeakers = [[NSMutableArray alloc] init];
+    
+    //nsarry *myArray = @[];
+    // ( )
+    
+    for (NSInteger i = 0; i <= [speakers count]-1; i++) {
+        
+        NSString *greetingAndRoomAssignmentForSpeaker = [NSString stringWithFormat:@"Welcome, %@! You'll be in dressing room %li.", speakers[i], i+1];
+        
+                   [greetingsAndRoomAssignmentsForSpeakers addObject:greetingAndRoomAssignmentForSpeaker];
+                }
+    return greetingsAndRoomAssignmentsForSpeakers;
+}
+
 @end
